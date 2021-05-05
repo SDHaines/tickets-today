d02f2a911255ebdf2f5a22f3f6c2b139cbb97025
SDHaines <scott.d.haines@gmail.com>
2021-05-05 13:24:37 -0400

    Added amount tendered to account for split payment transactions

--- ticketsToday.php
+++ ticketsToday.php
@@ -38,7 +38,8 @@ dbo.IM_ITEM.ADDL_DESCR_1,
 dbo.VI_INV_COMMIT_TRX.QTY_COMMIT AS QTY_SOLD,
 --dbo.IM_INV.QTY_AVAIL
 dbo.VI_PS_DOC_HDR.HDR_DISC_COD,
-dbo.PS_DOC_PMT.PAY_COD
+dbo.PS_DOC_PMT.PAY_COD,
+dbo.PS_DOC_PMT.AMT
 FROM VI_PS_DOC_HDR
 INNER JOIN dbo.VI_INV_COMMIT_TRX ON dbo.VI_INV_COMMIT_TRX.DOC_NO = dbo.VI_PS_DOC_HDR.TKT_NO
 INNER JOIN dbo.IM_ITEM ON dbo.IM_ITEM.ITEM_NO = dbo.VI_INV_COMMIT_TRX.ITEM_NO
@@ -47,7 +48,7 @@ INNER JOIN dbo.PS_DOC_PMT ON dbo.PS_DOC_PMT.TKT_NO = dbo.VI_PS_DOC_HDR.TKT_NO AN
 ORDER BY dbo.VI_PS_DOC_HDR.TKT_NO";
 
 print "---------------------------- " . date("Y-m-d H:m:s") . " ----------------------------\n";
-print "TKT_DT,TKT_NO,ITEM_NO,ADDL_DESCR_1,QTY_SOLD,HDR_DISC_CODE,PAY_COD\n";
+print "TKT_DT,TKT_NO,ITEM_NO,ADDL_DESCR_1,QTY_SOLD,HDR_DISC_CODE,PAY_COD,AMT\n";
 
 $status = counterpointQuickQuery( $tsql, "printRow" );
 
@@ -58,7 +59,8 @@ function printRow ( $row ){
     "\"" . $row['ADDL_DESCR_1'] . "\"" . "," .
     sprintf( "%d", $row['QTY_SOLD'] ) . "," .
     $row['HDR_DISC_COD'] . "," .
-    $row['PAY_COD'] . 
+    $row['PAY_COD'] . "," . 
+    $row['AMT'] . 
     "\n";
 }
 print "-----------------------------------------------------------------------------\n";
